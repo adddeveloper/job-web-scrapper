afab7ca5656ea16fad357731d40feb67cd5d4ab0
Adnan <77362547+adddeveloper@users.noreply.github.com>
2023-03-22 20:33:17 -0300

    update

---
+++ dist/two.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import time
+import json
+import os
+from bs4 import BeautifulSoup
+
+filename = os.path.basename(__file__)
+
+url = 'https://www.ironfoxgames.com/careers'
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(5)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+# title
+title = soup.find('title').get_text()
+divs = soup.find_all('div', class_='wixui-box')
+
+arr = []
+
+for div in divs:
+  arrone = {}
+  # h4
+  h4 = div.find_all("h4")
+  if h4:
+    for h in h4:
+      span = h.find("span")
+      span = span.text
+      if span:
+        arrone["job"] = span
+  # paragraph
+  p = div.find("p")
+  if p:
+    span = p.find("span")
+    span = span.text
+    if span:
+        arrone["discription"]=(span)
+  # anchor
+  anchor = div.find("a", class_="wixui-button")
+  if anchor:
+    anchor = anchor['href']
+    arrone["link"] = (anchor)
+  if arrone != {}:
+    arr.append(arrone)
+
+# print(arr, len(arr))
+with open("data/"+filename.split('.')[0]+'.json', "w") as file:
+    json.dump(arr, file)
+
+driver.quit()
---
+++ update.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+dir_ = os.path.abspath('.')
+folder = os.path.join(dir_, 'dist')
+
+li = os.listdir(folder)
+
+if not os.path.exists('/data'):
+    os.makedirs('/data')
+
+for i in li:
+    filepath = os.path.join('data', i.split('.')[0]+'.json')
+    with open(filepath, "w") as file:
+        pass
+    subprocess.run(['python', (folder+'/'+i)])
